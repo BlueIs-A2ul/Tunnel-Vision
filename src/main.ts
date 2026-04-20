835343a5632c4f5a03cfbdfc6301838ba5f9ebe0
BlueIs-A2ul <1738348581@qq.com>
2026-04-20 22:15:30 +0800

    安装项目依赖：Element Plus、TailwindCSS、Pinia、Vue Router、Axios

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app').$nextTick(() => {
-  // Use contextBridge
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app').$nextTick(() => {
   window.ipcRenderer.on('main-process-message', (_event, message) => {
     console.log(message)
   })
