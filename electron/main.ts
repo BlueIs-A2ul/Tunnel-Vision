2a1e443dde67ef18c1e48d9d31ea14f357d50a23
BlueIs-A2ul <1738348581@qq.com>
2026-04-22 20:22:16 +0800

    配置Electron窗口移除菜单栏，安装ECharts图表库

--- electron/main.ts
+++ electron/main.ts
@@ -34,6 +34,9 @@ function createWindow() {
     },
   })
 
+  // 完全移除菜单栏
+  win.setMenu(null)
+
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', (new Date).toLocaleString())
